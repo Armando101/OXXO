6adbe9d77cf7ef5ff14a5510c4a5ff946e2dde99
Armando <rivera.armando997@gmail.com>
2018-04-25 23:53:49 -0500

    OXXO

---
+++ js/add.js
@@ -0,0 +1,9 @@
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.parallax').parallax()
+    $('.slider').slider();
+  });
+
+ function Ejecuta() {
+ 	alert("Mensaje enviado, Tus comentarios nos ayudan a seguir creciendo")
+ }
